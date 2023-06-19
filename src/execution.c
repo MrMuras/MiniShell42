b7faca4018f721c0ea150556ef346d5628b1d75d
snocita <snocita@student.42wolfsburg.de>
2023-06-19 13:26:24 +0200

    added execve comment

--- src/execution.c
+++ src/execution.c
@@ -6,7 +6,7 @@
 /*   By: snocita <snocita@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 17:06:25 by snocita           #+#    #+#             */
-/*   Updated: 2023/06/19 13:22:47 by snocita          ###   ########.fr       */
+/*   Updated: 2023/06/19 13:26:01 by snocita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 void	execution(char	*input, t_cmd	*cmd)
 {
-	execve("/usr/bin/make", (char *[]){"make", "re", NULL}, (char *[]){NULL});
+	//execve("/usr/bin/make", (char *[]){"make", "re", NULL}, (char *[]){NULL});
 }
